f3c8c34ad08e03b03f33d0af57b3e5676f8d93cb
Anjila38 <anjilapahari38@gmail.com>
2024-12-01 12:14:45 +0545

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 module.exports = {
   content: ["./index.html"], // Scan the HTML file for classes
   theme: {
-    extend: {},
+    extend: {
+      
+    },
     screens: {
-      'sm': {'max': '639px'}, // Target screens up to 639px (phones)
+      sm: { max: "639px" }, // Target screens up to 639px (phones)
     },
   },
   plugins: [],
-}
+};
